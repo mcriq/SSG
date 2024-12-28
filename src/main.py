ffd948142e50133f8953350ff3da46a8dc2ee686
Matthew Criqui <mcriqui@MacBookPro.attlocal.net>
2024-12-28 13:15:52 -0600

    adding script

--- src/main.py
+++ src/main.py
@@ -1,2 +1,7 @@
-./main.sh
+from textnode import TextNode, TextType
 # hello world
+def main():
+  new_node = TextNode("This is a textnode", TextType.BOLD, "https://www.boot.dev")
+  print(new_node)
+
+main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,7 +1,25 @@
 from enum import Enum
 
-class Bender(enum):
-  AIR_BENDER = 'air'
-  WATER_BENDER = 'water'
-  EARTH_BENDER = 'earth'
-  FIRE_BENDER = 'fire'
+class TextType(Enum):
+  NORMAL = 'normal'
+  BOLD = 'bold'
+  ITALIC = 'italic'
+  CODE = 'code'
+  LINK = 'link'
+  IMAGE = 'image'
+
+
+class TextNode:
+  def __init__(self, text, text_type, url=None):
+    self.text = text
+    self.text_type = text_type
+    self.url = url
+
+  def __eq__(self, other):
+    if isinstance(self, other):
+      if self.text == other.text and self.text_type == other.text_type and self.url == other.url:
+        return True
+    return False
+  
+  def __repr__(self):
+    return f"TEXTNODE({self.text}, {self.text_type.value}, {self.url})"
