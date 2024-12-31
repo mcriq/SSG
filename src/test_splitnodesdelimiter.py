974e0c45be3273c8ef69eca2d78cab98b986fe41
Matthew Criqui <mcriqui@MacBookPro.attlocal.net>
2024-12-31 08:35:33 -0600

    rename inline markdown file and add tests

--- src/test_splitnodesdelimiter.py
+++ src/test_splitnodesdelimiter.py
@@ -1,6 +1,6 @@
 import unittest
 from textnode import TextNode, TextType
-from splitnodesdelimiter import split_nodes_delimiter
+from inline_markdown import split_nodes_delimiter
 
 
 class TestSplitNodesDelimiter(unittest.TestCase):
@@ -14,29 +14,48 @@ class TestSplitNodesDelimiter(unittest.TestCase):
   def test_split_nodes_multi_delim(self):
     node = TextNode("Text with `multiple` different `code blocks`", TextType.TEXT)
     nodes = split_nodes_delimiter([node], "`", TextType.CODE)
-    self.assertEqual(len(nodes), 4)
-    self.assertEqual(nodes[0].text, "Text with ")
-    self.assertEqual(nodes[0].text_type, TextType.TEXT)
-    self.assertEqual(nodes[1].text, "multiple")
-    self.assertEqual(nodes[1].text_type, TextType.CODE)
-    self.assertEqual(nodes[2].text, " different ")
-    self.assertEqual(nodes[2].text_type, TextType.TEXT)
-    self.assertEqual(nodes[3].text,  "code blocks")
-    self.assertEqual(nodes[3].text_type, TextType.CODE)
+    self.assertListEqual(
+      [
+        TextNode("Text with ", TextType.TEXT),
+        TextNode("multiple", TextType.CODE),
+        TextNode(" different ", TextType.TEXT),
+        TextNode("code blocks", TextType.CODE),
+      ],
+      nodes
+    )
 
   def test_split_nodes_not_text_type(self):
     node = TextNode("This is a code block", TextType.CODE)
     nodes = split_nodes_delimiter([node], "`", TextType.CODE)
-    self.assertEqual(len(nodes), 1)
-    self.assertEqual(nodes[0].text, "This is a code block")
-    self.assertEqual(nodes[0].text_type, TextType.CODE)
+    self.assertListEqual(
+      [
+        TextNode("This is a code block", TextType.CODE),
+      ],
+      nodes
+    )
 
   def test_split_nodes_empty_string(self):
     node = TextNode("", TextType.TEXT)
     nodes = split_nodes_delimiter([node], "`", TextType.TEXT)
-    self.assertEqual(len(nodes), 1)
-    self.assertEqual(nodes[0].text, "")
-    self.assertEqual(nodes[0].text_type, TextType.TEXT)
+    self.assertListEqual(
+      [
+        TextNode("", TextType.TEXT)
+      ],
+      nodes
+    )
+  
+  def test_delim_bold_and_italic(self):
+    node = TextNode("**bold** and *italic*", TextType.TEXT)
+    new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+    new_nodes = split_nodes_delimiter(new_nodes, "*", TextType.ITALIC)
+    self.assertListEqual(
+        [
+            TextNode("bold", TextType.BOLD),
+            TextNode(" and ", TextType.TEXT),
+            TextNode("italic", TextType.ITALIC),
+        ],
+        new_nodes,
+    )
 
   def test_split_nodes_unmatched_delim(self):
     with self.assertRaises(Exception):
