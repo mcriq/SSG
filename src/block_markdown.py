da00a24c3257d309c3250c161669c8e9f1e00703
mcriq <125591030+mcriq@users.noreply.github.com>
2025-01-02 16:03:14 -0600

    add block typing function

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -6,3 +6,54 @@ def markdown_to_blocks(markdown):
     block_list_stripped = list(map(lambda block: block.strip(), block_list))
     block_list_space_rem = [block for block in block_list_stripped if block]
     return block_list_space_rem
+
+def block_to_block_type(md_block):
+    if md_block[0] == "#":
+        count = 0
+        for char in md_block:
+            if char != "#":
+                break
+            if count < 6:
+                count += 1
+        return f"heading{count}"
+
+    if md_block[:3] == "```" and md_block[-3:] == "```":
+        return "code"
+
+    if md_block[0] == ">":
+        is_quote = True
+        for block in md_block.split("\n"):
+            if block.startswith(">"):
+                continue
+            else:
+                is_quote = False
+                break
+        if is_quote:
+            return "quote"
+    
+    if md_block[0] == "*" or md_block[0] == "-":
+        is_unordered_list = True
+        for block in md_block.split("\n"):
+            if block.startswith("*") or block.startswith("-"):
+                continue
+            else:
+                is_unordered_list = False
+                break
+        if is_unordered_list:
+            return "unordered list"
+    
+    if md_block[:3] == "1. ":
+        is_ordered_list = True
+        curr_digit = 0
+        for block in md_block.split("\n"):
+            curr_digit += 1
+            if block[0].isdigit() and block[:3] == f"{curr_digit}. ":
+                continue
+            else:
+                is_ordered_list = False
+                break
+        if is_ordered_list:
+            return "ordered list"
+
+    else:
+        return "paragraph"
--- src/test_blockmarkdown.py
+++ src/test_blockmarkdown.py
@@ -1,5 +1,5 @@
 import unittest
-from block_markdown import markdown_to_blocks
+from block_markdown import markdown_to_blocks, block_to_block_type
 
 
 class TestBlockMarkdown(unittest.TestCase):
@@ -25,6 +25,32 @@ This is a paragraph of text. It has some **bold** and *italic* words inside of i
      markdown = ""
      block_list = markdown_to_blocks(markdown)
      self.assertListEqual(block_list, [])
+
+  def test_block_to_block_type_heading(self):
+     md_block = "### Title"
+     self.assertEqual(block_to_block_type(md_block), "heading3")
+  
+  def test_block_to_block_type_code(self):
+     md_block = "```This is a code block```"
+     self.assertEqual(block_to_block_type(md_block), "code")
+
+  def test_block_to_block_type_quote(self):
+      md_block = ">this quote block is a\n>quote is a\n>quote"
+      self.assertEqual(block_to_block_type(md_block), "quote")
+
+  def test_block_to_block_type_unordered_list(self):
+      md_block = "*List item 1\n*List item 2\n*List item 3"
+      self.assertEqual(block_to_block_type(md_block), "unordered list")
+
+  def test_block_to_block_type_ordered_list(self):
+      md_block = "1. List item\n2. List item\n3. List item"
+      self.assertEqual(block_to_block_type(md_block), "ordered list")
+  
+  def test_block_to_block_type_paragraph(self):
+     md_block = "This is some plain old text"
+     self.assertEqual(block_to_block_type(md_block), "paragraph")
+  
+
 
 if __name__ == "__main__":
     unittest.main()
