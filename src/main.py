d52750e1cab2d8359e45b904acecc3ae3cab08ca
mcriq <125591030+mcriq@users.noreply.github.com>
2025-01-03 19:30:58 -0600

    add dir copy functionality for public folder

--- src/main.py
+++ src/main.py
@@ -1,7 +1,31 @@
+import os
+import shutil
 from textnode import TextNode, TextType
-# hello world
+
+def copy_content(source_path, dest_path):
+  if not os.path.exists(source_path):
+    raise Exception("Cannot copy from non-existent directory")
+  if os.path.exists(dest_path):
+    print(f'Deleting {dest_path} folder and contents...')
+    shutil.rmtree(dest_path)
+  os.mkdir(dest_path)
+  source_dir = os.listdir(source_path)
+  for item in source_dir:
+    source_full_path = os.path.join(source_path, item)
+    dest_full_path = os.path.join(dest_path, item)
+    if os.path.isfile(source_full_path):
+      shutil.copy(source_full_path, dest_full_path)
+      print(f"Copying file {source_full_path} to {dest_full_path}")
+    else:
+      os.mkdir(dest_full_path)
+      copy_content(source_full_path, dest_full_path)
+
+
+
+
 def main():
-  new_node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
-  print(new_node)
+  copy_content("static", "public")
 
 main()
+  # new_node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
+  # print(new_node)
